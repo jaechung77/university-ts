a52466251ed3609a20ba39776a6019361e54ed29
Jae Chung <jaechung77@gmail.com>
2022-06-24 23:08:48 +1000

    setup button component

---
+++ src/components/button/button.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Button = (props) => <button>Button</button>;
+
+export default Button;
--- src/components/buttons/buttons.jsx
+++ src/components/buttons/buttons.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import styles from './buttons.module.css';
+import Button from '../button/button';
 
-const Buttons = (props) => <h1>Buttons</h1>;
+const Buttons = (props) => (
+  <ul className={styles.ul}>
+    <li>
+      <Button className={styles.btn__load} />
+    </li>
+    <li>
+      <Button className={styles.btn__delete} />
+    </li>
+    <li>
+      <Button className={styles.btn__add} />
+    </li>
+  </ul>
+);
 
 export default Buttons;
