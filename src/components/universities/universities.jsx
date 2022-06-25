618d3856abe2d80a150d800bbb337d3cff29cc61
Jae Chung <jaechung77@gmail.com>
2022-06-26 02:46:44 +1000

    add university component & print uni name

--- src/components/universities/universities.jsx
+++ src/components/universities/universities.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import University from '../university/university';
 
-const Universities = (props) => <h1>Universities</h1>;
+const Universities = (props) => {
+  const universities = useSelector(
+    (state) => state.getUniversities.universities
+  );
+  const renderUnis = universities.map((uni, i) => {
+    const { name } = uni;
+
+    return (
+      <div>
+        <ul>{uni.name && <University name={name} />}</ul>
+      </div>
+    );
+  });
+  return <>{renderUnis}</>;
+};
 
 export default Universities;
---
+++ src/components/university/university.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const University = ({ name }) => <li>{name}</li>;
+
+export default University;
