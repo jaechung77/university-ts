9e2bb49194a1efccf83c037d80066c3d46f46582
Jae Chung <jaechung77@gmail.com>
2022-06-26 14:14:49 +1000

    refactor universities component

--- src/components/universities/universities.jsx
+++ src/components/universities/universities.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import University from '../university/university';
+import styles from './universities.module.css';
 
 const Universities = (props) => {
   const universities = useSelector(
@@ -8,29 +9,15 @@ const Universities = (props) => {
   );
 
   return (
-    <>
-      <div>
-        <ul>
-          {universities.map((uni, i) => {
-            const { domains, web_pages, name, country, alpha_two_code } = uni;
-            return (
-              <li key={i}>
-                {uni.name && ( //
-                  <University
-                    name={name}
-                    domains={domains}
-                    web_pages={web_pages}
-                    name={name}
-                    country={country}
-                    alpha_two_code={alpha_two_code}
-                  />
-                )}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </>
+    <section className={styles.container}>
+      <ul className={styles.cards}>
+        {universities.map((uni, i) => {
+          uni && ( //
+            <University uni={uni} key={i} />
+          );
+        })}
+      </ul>
+    </section>
   );
 };
 
--- src/components/university/university.jsx
+++ src/components/university/university.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import styles from './university.module.css';
 
-const University = ({ domains, web_pages, name, country, alpha_two_code }) => (
-  <>
-    <h1>{name}</h1>
-    <p>Domains: {domains}</p>
-    <p>Country: {country}</p>
-    <p>Webpage: {web_pages}</p>
-    <p>Country Code: {alpha_two_code}</p>
-  </>
-);
+const University = ({ uni, key }) => {
+  const { domains, web_pages, name, country, alpha_two_code } = uni;
+  return (
+    <li className={styles.card} key={key}>
+      <h1>{name}</h1>
+      <p>Domains: {domains}</p>
+      <p>Country: {country}</p>
+      <p>Webpage: {web_pages}</p>
+      <p>Country Code: {alpha_two_code}</p>
+    </li>
+  );
+};
 export default University;
